a6c4f89f0c87e2d84c59392e5fd9b8fad566a636
jeon <foodchain@naver.com>
2023-01-22 00:11:43 +0900

    광고문자 > 단문*장문 화면 및 메인 모달 팝업 작업

--- v2/assets/js/ui.js
+++ v2/assets/js/ui.js
@@ -8,6 +8,7 @@ $(function () {
   // ui 인터렉션 호출
   cmmnUi.init();
   cmmnUi.modal();
+  cmmnUi.userForm();
 });
 
 const cmmnUi = {
@@ -34,5 +35,25 @@ const cmmnUi = {
       });
       return false;
     });
+  },
+  userForm() {
+    $('.radio-chk-display input').on('change', function () {
+      isChked($('[data-ui="divide-used"]'), $('[data-ui="divide-unused"]'));
+      isChked($('[data-ui="divide-unused"]'), $('[data-ui="divide-used"]'));
+      isChked($('[data-ui="time-used"]'), $('[data-ui="time-unused"]'));
+      isChked($('[data-ui="time-unused"]'), $('[data-ui="time-used"]'));
+    });
+
+    // 디폴트 체크 값 설정
+    $('.user-form-choice > .radio-chk-display input').prop('checked', true).trigger('change');
+
+    function isChked($targetEl, $anotherEl) {
+      if ($targetEl.is(':checked')) {
+        $targetEl.prop('checked', true).parent().removeClass('disabled');
+        $targetEl.parent().siblings('input').attr('disabled', false);
+        $anotherEl.prop('checked', false).parent().addClass('disabled');
+        $anotherEl.parent('.disabled').siblings('input').attr('disabled', true);
+      }
+    }
   },
 };
